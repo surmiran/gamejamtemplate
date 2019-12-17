b17d8f4f0b572756d46d7cdef856843d2d3224df
Curdin Pitsch <curdin.pitsch@wuerth-itensis.com>
2019-12-17 16:58:21 +0100

    CHANGE: added simple es6 game setup

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
 import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
+import GameScene from "./GameScene"
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
+  // parent: "phaser-example",
   width: 800,
   height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+  scene: [GameScene]
 };
 
-const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("logo", logoImg);
-}
-
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
+new Phaser.Game(config);
