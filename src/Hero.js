37796600db95db77ed1505164a49104fa0cb79e3
Curdin Pitsch <curdin.pitsch@wuerth-itensis.com>
2019-12-19 09:17:00 +0100

    FEATURE: created input controller

--- src/Hero.js
+++ src/Hero.js
@@ -4,66 +4,28 @@ import Constants from './Constants';
 export default class Hero extends Phaser.GameObjects.Image {
 	constructor(world) {
 		super(world.scene, Constants.spawnX * Constants.tileSize + Constants.tileSize / 2, Constants.spawnY * Constants.tileSize + Constants.tileSize / 2, 'car');
-		console.log('create hero');
+	}
 
-		//  Left
-		world.scene.input.keyboard.on('keydown_A', (event) => {
-
-			var tile = world.layer.getTileAtWorldXY(this.x - Constants.tileSize, this.y, true);
-
-			if (tile.index === Constants.blockedTileID) {
-				//  Blocked, we can't move
-			}
-			else {
+	move(direction) {
+		let angle = this.angle;
+		switch (direction) {
+			case Phaser.LEFT:
 				this.x -= Constants.tileSize;
-				this.angle = 180;
-			}
-
-		});
-
-		//  Right
-		world.scene.input.keyboard.on('keydown_D', (event) => {
-
-			var tile = world.layer.getTileAtWorldXY(this.x + Constants.tileSize, this.y, true);
-
-			if (tile.index === Constants.blockedTileID) {
-				//  Blocked, we can't move
-			}
-			else {
+				angle = 180;
+				break;
+			case Phaser.RIGHT:
 				this.x += Constants.tileSize;
-				this.angle = 0;
-			}
-
-		});
-
-		//  Up
-		world.scene.input.keyboard.on('keydown_W', (event) => {
-
-			var tile = world.layer.getTileAtWorldXY(this.x, this.y - Constants.tileSize, true);
-
-			if (tile.index === Constants.blockedTileID) {
-				//  Blocked, we can't move
-			}
-			else {
+				angle = 0;
+				break;
+			case Phaser.UP:
 				this.y -= Constants.tileSize;
-				this.angle = -90;
-			}
-
-		});
-
-		//  Down
-		world.scene.input.keyboard.on('keydown_S', (event) => {
-
-			var tile = world.layer.getTileAtWorldXY(this.x, this.y + Constants.tileSize, true);
-
-			if (tile.index === Constants.blockedTileID) {
-				//  Blocked, we can't move
-			}
-			else {
+				angle = -90;
+				break;
+			case Phaser.DOWN:
 				this.y += Constants.tileSize;
-				this.angle = 90;
-			}
-
-		});
+				angle = 90;
+				break;
+		}
+		this.angle = angle;
 	}
 }
