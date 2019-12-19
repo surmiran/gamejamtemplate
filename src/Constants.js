c1d0442d09f405aa2bbff85c49765278ba1a0554
Martin Fluor <martin.fluor@wuerth-itensis.com>
2019-12-19 11:55:07 +0100

    Added heinz asset and victory tile ids

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,7 @@
 export default {
 	tileSize: 32,
 	blockedTileIDs: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 17],
+	victoryTileIDs: [18, 19, 20, 21, 22, 23],
 	upgradeTileID: 26,
 	trapTileID: 25,
 	spawnTileID: 24,
